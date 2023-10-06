3b1b87d8ae8f3f0e86df453a3c25e07dfdc91fce
Maurício Lorenzetti <mauricio.lorenzetti.bezerra@gmail.com>
2023-10-05 21:27:10 -0300

    Update of server files

--- server/app.py
+++ server/app.py
@@ -1,16 +1,16 @@
+from flask_socketio import send, emit
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import eventlet
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
-
-last_heart_rate = None
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', heart_rate=last_heart_rate)
+
+last_heart_rate = None
 
 @socketio.on('connect')
 def handle_connect():
@@ -25,7 +25,7 @@ def handle_heart_rate(heart_rate):
     global last_heart_rate
     last_heart_rate = heart_rate
     print('Heart rate logged:', last_heart_rate)
-    socketio.emit('new_heart_rate', heart_rate)
+    socketio.emit('new_heart_rate', heart_rate+5)
 
 if __name__ == '__main__':
     socketio.run(app)
